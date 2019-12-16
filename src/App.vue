926cddfd1f53ef50f3df7e65c9567fee1064b0af
Victor Cherezov <victor_cherezov@mentor.com>
2019-12-16 16:38:38 +0300

    get books

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,16 @@
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+} */
+
+#app {
+  margin-top: 60px
 }
 
 #nav {
